d5b96203f4c7e21ebfe570650563a4803e400eb6
unknown <scottwesleybean@gmail.com>
2022-09-11 13:56:59 -0600

    Adding Mapping_GeoJSON_Points folder

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,95 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// // Grabbing our GeoJSON data
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// // Grabbing our GeoJSON data, using the pointToLayer function
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//         console.log(feature);
+//         return L.marker(latlng)
+//         .bindPopup("<h2>" + feature.properties.city + "</h2>");
+//     }
+// }).addTo(map);
+
+// Skill Drill: Popup should display, city, country, and name of airport
+// Grabbing our GeoJSON data, using the pointToLayer function
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//         console.log(feature);
+//         return L.marker(latlng)
+//         .bindPopup("<h3><b>" + feature.properties.name + "</b></h3><hr>"
+//         + "<h4>" + feature.properties.city + ", " + feature.properties.country + "</h4>");
+//     }
+// }).addTo(map);
+
+// // Grabbing our GeoJSON data, using the onEachFeature function
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer);
+//         layer.bindPopup();
+//     }
+// }).addTo(map);
+
+// Skill Drill: Popup should display airport code and airport name.
+// Grabbing our GeoJSON data, using the onEachFeature function
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h3><b>Airport code: " + feature.properties.faa + "</b></h3><hr>"
+        + "<h3><b>Airport name: " + feature.properties.name + "</b></h3>");
+    }
+}).addTo(map);
+
+// //We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// // Skill Drill: Night preview map
+// //We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// Skill Drill: Outdoors map
+//We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
